94a79f2e4d7933f3c4a05ff1539caa51e7e2de98
Lica Mihai Ionut <licamihai26@yahoo.ro>
2025-02-12 12:03:36 +0200

    4th commit

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -11,9 +11,10 @@ namespace API.Controllers;
 public class ProductsController(IProductRepository repo) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, 
+        string? type, string? sort)
     {
-        return Ok(await repo.GetProductsAsync());
+        return Ok(await repo.GetProductsAsync(brand, type, sort));
     }
 
     [HttpGet("id:int")]
@@ -70,6 +71,18 @@ public class ProductsController(IProductRepository repo) : ControllerBase
         }
 
         return BadRequest("Problem deleting the product");
+    }
+
+    [HttpGet("brands")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+    {
+        return Ok(await repo.GetBrandsAsync());
+    }
+
+    [HttpGet("types")]
+    public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+    {
+        return Ok(await repo.GetTypesAsync());
     }
 
     private bool ProductExists(int id)
